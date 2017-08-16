cdcc8286b4fc9927815d3a5c3f9823bce78896cc
= <=>
2017-08-16 13:25:19 +0530

    LP3HW - 1st exercise

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("Yay! Printing....!")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("This is the last line.....See you in a while mate!")
+#Single Line Comment in Python.
+#print("Bye!")
