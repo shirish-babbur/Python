b1aeaae5881796fc0b4563ef13274240a6670a21
shirish-babbur <bbshirish@gmail.com>
2017-08-16 16:03:14 +0530

    LP3HW - 4th exercise - Added study drills

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+# Number of Cars.
 cars = 100
-space_in_a_car = 4.0
+# Study-drill Updations.
+#space_in_a_car = 4.0
+# Number of drivers.
 drivers = 30
+# Number of passengers.
 passengers = 90
+# Number of cars which are placed in junkyard.
 cars_not_driven = cars - drivers
+ # Number of cars driven.
 cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
+#Carpool capacity.
+carpool_capacity = cars_driven * 4.0 #Study drill Updations.
+# Number of passengers in one car.
 average_passengers_per_car = passengers / cars_driven
 
 print("There are", cars ,"cars available.")
