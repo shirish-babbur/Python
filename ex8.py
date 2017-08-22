6fc76db330685cea727929f7ab2394a590bd5dfa
shirish-babbur <bbshirish@gmail.com>
2017-08-22 17:05:20 +0530

    LP3HW - 8th exercise

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format(True, False, False, True))
+print(formatter.format("One", "Two", "Three", "Four"))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Hey Bro",
+    "You've Earned it",
+    "Don't ever fake it",
+    "You have it now! Enjoy!"
+    ))
