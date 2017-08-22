a9655d3abc0570b64c3bb5c8345c28231c10af40
shirish-babbur <bbshirish@gmail.com>
2017-08-22 17:54:51 +0530

    LP3HW - 10th exercise - Added study drills.

--- ex10.py
+++ ex10.py
@@ -2,14 +2,23 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat_double = """
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
+'''This is IT'''
 """
+fat_cat_single = '''
+I'll do a list:
+\t* "Cat food"
+\t* "Fishies"
+\t* "Catnip"\n\t* "Grass"
+"""This is Why!"""
+'''
 
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
-print(fat_cat)
+print(fat_cat_single)
+print(fat_cat_double)
