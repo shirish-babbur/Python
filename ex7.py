3be77d16b9e2d2a9e49ff477876ca31dc1c435bb
shirish-babbur <bbshirish@gmail.com>
2017-08-22 16:58:55 +0530

    LP3HW - 7th exercise - Added Study drill.

--- ex7.py
+++ ex7.py
@@ -1,7 +1,9 @@
+# exercise for more printing practices.
 print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
 print("And Everywhere that Mary went.")
-print("."*10) # What'd that do? It will print '.' char 10 times.
+#print char '.' 10 times
+print("."*10) # What'd that do? Answer - It will print '.' char 10 times.
 
 end1 = "C"
 end2 = "h"
@@ -17,5 +19,6 @@ end11 = "e"
 end12 = "r"
 
 # Watch that comma at the end. try removing it to see what happens.
+# 'end' is simple parameter which decides what has to append after the input passed to print function.It's default value is '\n'.We can put any other char as below. 
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 +end12)
