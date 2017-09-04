99c07e813f90111921fafeac9d2583e5e5f30487
shirish-babbur <bbshirish@gmail.com>
2017-09-04 15:24:58 +0530

    LP3HW - 15th exercise  - Added study drill

--- ex15.py
+++ ex15.py
@@ -1,15 +1,26 @@
+# Imports argv from the sys module.
 from sys import argv
 
+# unpacks values from argv (command line) into scrit and filename.
 script, filename = argv
 
+# opens file in the default read mode.
 txt = open(filename)
 
+# Prints name and contents of the file.
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+# Takes input from user in the form of filename.
 print("Type the filename again:")
 file_again = input("> ")
 
+# Opens the specified file in the default read mode.
 txt_again = open(file_again)
 
+# Prints the contents of the file.
 print(txt_again.read())
+
+# Close opened file objects.
+txt.close()
+txt_again.close()
