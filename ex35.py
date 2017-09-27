cd74cf747cf3dc04352a5ba11d8a4abd51981939
shirish-babbur <bbshirish@gmail.com>
2017-09-27 14:48:21 +0530

    LP3HW - 35th exercise -Added study drills

--- ex35.py
+++ ex35.py
@@ -4,7 +4,7 @@ def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice = input("> ")
-    if "0" in choice or "1" in choice:
+    if choice.isnumeric():
         how_much = int(choice)
     else:
         dead("Man, learn to type number.")
@@ -46,6 +46,7 @@ def cthulhu_room():
     choice = input("> ")
 
     if "flee" in choice:
+        print("Coward Spotted!")
         start()
     elif "head" in choice:
         dead("Well that was tasty!")
