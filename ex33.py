0a6f1b150dda0626a5d51a0bc860341d7de3fd2d
shirish-babbur <bbshirish@gmail.com>
2017-09-27 11:15:43 +0530

    LP3HW - 33rd exercise - Added study drill

--- ex33.py
+++ ex33.py
@@ -1,15 +1,17 @@
-i = 0
 numbers = []
+i = 0
+def loopthrough(limit, incrementby):
+    #i = 0
+    global i
+    while i < limit:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
-
-    i = i + 1
-    print("Numbers Now: ",numbers)
-    print(f"At the bottom i is {i}")
+        i = i + incrementby
+        print("Numbers Now: ",numbers)
+        print(f"At the bottom i is {i}")
 
 print("The numbers: ")
-
+loopthrough(10,2)
 for num in numbers:
     print(num)
