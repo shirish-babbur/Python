ac55458d8d18a205e0512c49969666659b5e2355
shirish-babbur <bbshirish@gmail.com>
2017-09-27 11:58:16 +0530

    LP3HW - 34th exercise

---
+++ ex34.py
@@ -0,0 +1,9 @@
+# Trying index of lists.
+
+data = ['Truck', 'Car', 'bike', 'walk']
+
+print(f"Element at zeroth cardinal i.e 1st ordinal is {data[0]}")
+i = 0
+while(i < len(data)):
+    print(f"Element at index {i} is {data[i]}")
+    i +=1 #Python doesn't have increment operation
