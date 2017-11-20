18c7d2cf46977b338512957aa87fb1d2dd1a1097
shirish-babbur <bbshirish@gmail.com>
2017-11-20 12:25:39 +0530

    LP3HW - 47th exercise

---
+++ projects/ex47/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+config = {
+    'description' : 'My First Project In Python',
+    'author' : 'Shirish Babbur',
+    'url' : 'Soon!',
+    'download_url' : 'https://abc.com/',
+    'author_email' : 'bbshirish@gmail.com',
+    'version' : '0.1',
+    'install_requires' : ['nose'],
+    'test_suite' : 'nose.collector',
+    'test_require' : ['nose'],
+    'packages' : ['Demo'],
+    'scripts' : ['bin/starter-engine'],
+    'name' : 'Demo Project'
+}
+
+setup(**config)
