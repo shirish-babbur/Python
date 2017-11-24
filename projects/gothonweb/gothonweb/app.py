e01c3eef9bf465f8567900f1d64c79dc53b270d1
shirish-babbur <bbshirish@gmail.com>
2017-11-24 11:49:12 +0530

    LP3HW - 51st exercise

--- projects/gothonweb/gothonweb/app.py
+++ projects/gothonweb/gothonweb/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    greeting = 'YO! How\'s you doing'
-    return render_template("index.html", greeting = greeting)
+@app.route('/hello', methods=['POST', 'GET'])
+def index():
+    greeting = "Hello world"
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template('index.html', greeting=greeting)
+    else:
+        return render_template("Hello_form.html")
 
 @app.route('/foo')
 def got():
